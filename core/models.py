95be8a432eb88d1a850739dde1ef560e95238d64
woohoocheezy <woohoocheezy@gmail.com>
2021-09-02 23:54:00 +0900

    Rooms and Core modeling
    creating rooms modeling and core modeling

---
+++ core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+
+    """Time Stamped Model"""
+
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from users import models as user_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+    """Abstract Item"""
+
+    name = models.CharField(max_length=80, default="")
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    """RoomType Model Definition"""
+
+    class Meta:
+        verbose_name = "Room Types"
+
+
+class Amenity(AbstractItem):
+    """Amenity Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+    """Facility Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+    """HouseRule Model Definition"""
+
+    class Meta:
+        verbose_name = "House Rules"
+
+
+class Photo(core_models.TimeStampedModel):
+    """Photo Model Definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
+
+class Room(core_models.TimeStampedModel):
+
+    """Room Model Definition"""
+
+    name = models.CharField(max_length=140, null=True)
+    description = models.TextField(default="")
+    country = CountryField(null=True)
+    city = models.CharField(max_length=80, null=True)
+    price = models.IntegerField(null=True)
+    address = models.CharField(max_length=140, null=True)
+    guests = models.IntegerField(null=True)
+    beds = models.IntegerField(null=True)
+    bedrooms = models.IntegerField(null=True)
+    baths = models.IntegerField(null=True)
+    check_in = models.TimeField(null=True)
+    check_out = models.TimeField(null=True)
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE, default="")
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField("Facility")  # processing as string is fine
+    house_rules = models.ManyToManyField(HouseRule)
+
+    def __str__(self):
+        return self.name
